cdaa9b56e77ad135c666631927e3676ca708ec72
Jerry Kwok <jerrykwok.khh@gmail.com>
2024-08-12 03:13:25 +0800

    fix: icons stroke props

--- src/components/icons/HomeIcon/index.tsx
+++ src/components/icons/HomeIcon/index.tsx
@@ -7,9 +7,9 @@ export const HomeIcon = ({
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       {...restProps}
     >
       <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8" />
--- src/components/icons/MenuIcon/index.tsx
+++ src/components/icons/MenuIcon/index.tsx
@@ -9,9 +9,9 @@ export const MenuIcon = ({
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       {...restProps}
     >
       <line x1="4" x2="20" y1="12" y2="12" />
