fb67c787b0364529edf108a6055cc7b95468d053
jerrykhh <jerrykwok.khh@gmail.com>
2024-09-01 02:11:10 +0800

    feat: typography compontents

---
+++ src/components/ui/typography.tsx
@@ -0,0 +1,130 @@
+import { forwardRef } from 'react';
+
+import { cn } from '@/lib/utils';
+
+export const H1 = forwardRef<
+  HTMLHeadingElement,
+  React.HTMLAttributes<HTMLHeadingElement>
+>(({ className, ...props }, ref) => {
+  return (
+    <h1
+      ref={ref}
+      className={cn(
+        'scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl',
+        className
+      )}
+      {...props}
+    />
+  );
+});
+H1.displayName = 'H1';
+
+export const H2 = forwardRef<
+  HTMLHeadingElement,
+  React.HTMLAttributes<HTMLHeadingElement>
+>(({ className, ...props }, ref) => {
+  return (
+    <h2
+      ref={ref}
+      className={cn(
+        'scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0',
+        className
+      )}
+      {...props}
+    />
+  );
+});
+H2.displayName = 'H2';
+
+export const H3 = forwardRef<
+  HTMLHeadingElement,
+  React.HTMLAttributes<HTMLHeadingElement>
+>(({ className, ...props }, ref) => {
+  return (
+    <h3
+      ref={ref}
+      className={cn(
+        'scroll-m-20 text-2xl font-semibold tracking-tight',
+        className
+      )}
+      {...props}
+    />
+  );
+});
+H3.displayName = 'H3';
+
+export const H4 = forwardRef<
+  HTMLHeadingElement,
+  React.HTMLAttributes<HTMLHeadingElement>
+>(({ className, ...props }, ref) => {
+  return (
+    <h4
+      ref={ref}
+      className={cn(
+        'scroll-m-20 text-xl font-semibold tracking-tight',
+        className
+      )}
+      {...props}
+    />
+  );
+});
+H4.displayName = 'H4';
+
+export const P = forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLParagraphElement>
+>(({ className, ...props }, ref) => {
+  return (
+    <p
+      ref={ref}
+      className={cn('leading-7 [&:not(:first-child)]:mt-6', className)}
+      {...props}
+    />
+  );
+});
+P.displayName = 'P';
+
+export const Blockquote = forwardRef<
+  HTMLQuoteElement,
+  React.HTMLAttributes<HTMLQuoteElement>
+>(({ className, ...props }, ref) => {
+  return (
+    <blockquote
+      ref={ref}
+      className={cn('mt-6 border-l-2 pl-6 italic', className)}
+      {...props}
+    />
+  );
+});
+Blockquote.displayName = 'Blockquote';
+
+export const InlineCode = forwardRef<
+  HTMLSpanElement,
+  React.HTMLAttributes<HTMLSpanElement>
+>(({ className, ...props }, ref) => {
+  return (
+    <code
+      ref={ref}
+      className={cn(
+        'relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold',
+        className
+      )}
+      {...props}
+    />
+  );
+});
+InlineCode.displayName = 'InlineCode';
+
+export const Lead = forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLParagraphElement>
+>(({ className, ...props }, ref) => {
+  return (
+    <p
+      ref={ref}
+      className={cn('text-xl text-muted-foreground', className)}
+      {...props}
+    />
+  );
+});
+Lead.displayName = 'Lead';
