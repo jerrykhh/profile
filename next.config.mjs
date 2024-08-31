1d0502b8df2e8d8d3a5631bcbac3cb1233cf42a5
jerrykhh <jerrykwok.khh@gmail.com>
2024-08-31 18:56:47 +0800

    fix: allow all https images

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,15 @@ const withNextIntl = createNextIntlPlugin('./src/i18n/i18n.ts');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   distDir: 'dist/.next',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+    dangerouslyAllowSVG: true,
+  },
 };
 
 export default withNextIntl(nextConfig);
