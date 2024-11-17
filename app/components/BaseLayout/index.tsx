fca7a6c26249707453c61eb7028b76ec559b7f39
jerrykwok-viutv <47948629+jerrykhh@users.noreply.github.com>
2024-11-17 13:54:40 +0800

    fix: navbar fixed position

--- app/components/BaseLayout/index.tsx
+++ app/components/BaseLayout/index.tsx
@@ -9,14 +9,14 @@ export const BaseLayout = ({ children }: { children: React.ReactNode }) => {
       <div className="md:flex">
         <div
           className={cn(
-            'fixed left-0 bottom-0 h-14 flex w-full justify-center border-t bg-background',
-            'md:static md:bottom-auto md:flex-col md:justify-start md:max-w-16 md:border-r md:min-h-screen'
+            'fixed left-0 bottom-0 h-14 flex w-full justify-center border-t bg-background z-10',
+            'md:bottom-auto md:flex-col md:justify-start md:w-[var(--nav-narrow-width)] md:border-r md:min-h-screen'
           )}
         >
           <Navigation items={navItems} />
         </div>
-        <div className="grow">
-          <div className="flex flex-col items-center py-4">
+        <div className="w-full md:content md:ml-auto p-4">
+          <div className="flex flex-col items-center">
             <div className="max-w-5xl">{children}</div>
           </div>
         </div>
