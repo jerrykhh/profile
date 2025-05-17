cdd812cbf248db8d715095626e30199d15b80c28
jerrykhh <jerrykwok.khh@gmail.com>
2025-05-17 23:18:05 +0800

    fix: latest released version

--- src/components/Home/Module/ReleasedVersionList/index.tsx
+++ src/components/Home/Module/ReleasedVersionList/index.tsx
@@ -11,25 +11,32 @@ type ReleasedVersionParams = {
 
 export const ReleasedVersionList = ({ items }: ReleasedVersionParams) => {
   return (
-    <Card title={`Releases (${items.length})`} containerClassName="border p-4">
+    <Card
+      title={`Releases (${items.length})`}
+      containerClassName="border px-4 py-4"
+    >
       <ul>
-        {items.map((item) => {
+        {items.map((item, i) => {
+          // const isCurrentRelease = item.url ? new URL(item.url).hostname === new URL(pathname)
           const isProvidedUrl = R.isNotEmpty(item.url) && R.isNotNil(item.url);
+          const isLatestVersion = i === items.length - 1;
           const releaseEle = (
             <li
               key={item.tag}
               className={classNames(
-                'py-2 border-b',
-                isProvidedUrl
-                  ? 'hover:bg-neutral-800 cursor-pointer'
-                  : 'cursor-not-allowed'
+                'py-1 border-b',
+                !isLatestVersion
+                  ? isProvidedUrl
+                    ? 'hover:bg-neutral-800 cursor-pointer'
+                    : 'cursor-not-allowed'
+                  : 'cursor-pointer'
               )}
             >
               {' '}
               {item.tag}
             </li>
           );
-          return isProvidedUrl ? (
+          return isProvidedUrl && !isLatestVersion ? (
             <Link
               key={item.tag}
               to={item.url!}
